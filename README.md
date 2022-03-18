632bfe1e6e994051fd184fb644c2ae1b92b3267a
marigonza123 <marina.lopes2@fatec.sp.gov.br>
2022-03-18 20:54:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-a7zalf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-a7zalf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { JurosComponent } from './juros/juros.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: '', component: HomeComponent},
+    {path: 'juros', component: JurosComponent},
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, JurosComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
